0f5a339155a57455c084153d7e2a375671204a3c
codestar1225 <reactexpert1996@gmail.com>
2025-01-22 06:05:19 -0600

    fix: remove old mongodb connection attribute

--- backend/config/db/DbCon.js
+++ backend/config/db/DbCon.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const DbCon = async () => {
   try {
     await mongoose.set("strictQuery", false);
-    const con = await mongoose.connect(process.env.MONGO_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const con = await mongoose.connect(process.env.MONGO_URL);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit();
