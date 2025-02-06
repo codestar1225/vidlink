f9e8ff2dee97645662272edfec895b6aacc98434
codestar1225 <reactexpert1996@gmail.com>
2025-02-06 06:05:30 -0600

    fix: deploy error

--- frontend/src/app/(privte)/upload/_components/mobile/index.tsx
+++ frontend/src/app/(privte)/upload/_components/mobile/index.tsx
@@ -5,7 +5,7 @@ import { useVideoValidate } from "@/hooks/useVideoValidate";
 import dynamic from "next/dynamic";
 import Upload from "./upload";
 import Loading from "@/app/_components/ui/loading";
-import useUpload from "@/hooks/useUpload";
+// import useUpload from "@/hooks/useUpload";
 const AddCards = dynamic(() => import("./addCards"));
 const Preview = dynamic(() => import("./preview"));
 
@@ -13,16 +13,16 @@ const UploadMobile = () => {
   const [isUpload, setIsUpload] = useState<boolean>(false);
   const [isAdd, setIsAdd] = useState<boolean>(false);
   const { error, fileName, videoSrc, validateVideo } = useVideoValidate();
-  const { uploadVideo, loading } = useUpload();
+  // const { uploadVideo, loading } = useUpload();
 
   function handleUpload(e: ChangeEvent<HTMLInputElement>) {
     validateVideo(e);
   }
-  const videoUp = (file: ChangeEvent<HTMLInputElement>) => {
-    const video = new FormData();
-    if (file) {
-    }
-  };
+  // const videoUp = (file: ChangeEvent<HTMLInputElement>) => {
+  //   const video = new FormData();
+  //   if (file) {
+  //   }
+  // };
 
   return (
     <>
