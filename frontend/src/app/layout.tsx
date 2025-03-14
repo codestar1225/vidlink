cc3745f18e8c2227ecf8d7ef99640b1fc189d484
codestar1225 <reactexport1996@gmail.com>
2025-03-14 12:29:00 -0500

    feat: logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -35,6 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
